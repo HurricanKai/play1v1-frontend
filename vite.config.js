f69e856fa6808daa97c3ae5df75ddb21d59e6756
Kai Jellinghaus <contact@kaij.tech>
2023-11-10 13:58:12 +0100

    Add end-of-game to vite

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 arena: resolve(__dirname, "arena.html"),
+                "game-end": resolve(__dirname, "game-end.html"),
             },
         },
         target: "esnext"
